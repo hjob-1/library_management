eea05d889185ba170573199ca37d6043e8437804
eyob <hjob12tadele@gmail.com>
2024-10-16 07:01:40 +0300

    Project setup initial commit

---
+++ src/main/java/com/obsidi/library_management/App.java
@@ -0,0 +1,13 @@
+package com.obsidi.library_management;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
